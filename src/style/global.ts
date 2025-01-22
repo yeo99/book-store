8e2b8407a36a4449b82a798dd614582a2f178106
yeo99 <qw6375@gmail.com>
2025-01-22 19:33:48 +0900

    Feat: Styled component 추가, 전역 스타일 지정

---
+++ src/style/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components"
+import "sanitize.css"
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+    }
+    
+    h1 {
+        margin: 0;
+    }
+`;
